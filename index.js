eccdd3898fcafae7e3bd1d1a44280cb3c4633571
devmashum <dev.mashum@gmail.com>
2024-01-05 15:03:02 +0100

    practice js

---
+++ index.js
@@ -0,0 +1,33 @@
+// function name (a, b) {
+//     return a+b;
+// }
+
+// const result = name (2,3);
+// console.log(result);
+
+// const numbers = [2, 5, 6, 3, 7];
+
+// const result = numbers.find(f => f === 3);
+// console.log(result);
+
+//Array Method
+const Fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// Find Method: 
+
+// const result = Fruits.find((f)=> f === "Apple");
+// console.log(result);
+
+// FindIndex Method: 
+// const result = Fruits.findIndex((f)=> f === "Apple");
+// console.log(result);
+
+// Filter Method
+ 
+// const result = Fruits.filter((f)=> f === 'Lemon' || f === 'Orange');
+// console.log(result);
+
+// Slice Method
+ 
+const result = Fruits.slice(1,3)
+console.log(result);
+console.log(Fruits)
